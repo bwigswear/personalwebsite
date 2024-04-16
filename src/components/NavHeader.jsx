077ca473a0939cf52bd0d8fe27a0901a7c6d4d07
unknown <bwigginswtsf@gmail.com>
2024-04-15 22:35:02 -0400

    nav bar changes

--- src/components/NavHeader.jsx
+++ src/components/NavHeader.jsx
@@ -5,12 +5,17 @@ import NavButton from './NavButton';
 const NavHeader = () => {
     return (
         <nav className={classes.header}>
-            <ul>
+            <ul className="internal-links">
                 <NavButton text='Home'/>
+                <NavButton text='About'/>
                 <NavButton text='Contact'/>
                 <NavButton text='Projects'/>
-                <SettingsButton />
+            </ul>
+            <ul className="external-links">
+                {/*<SettingsButton />*/}
+                <NavButton icon='../../images/gmailicon.png' />
                 <NavButton icon='../../images/github-mark.png' />
+                <NavButton icon='../../images/LI-In-Bug.png' />
             </ul>
         </nav>
     );
