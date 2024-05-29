f3ab3f7869f38aff72010f1c8caa1ea2dd2952dc
unknown <bwigginswtsf@gmail.com>
2024-05-28 21:04:57 -0400

    working on front page effects

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { useState, useEffect } from 'react';
 import { Outlet, useLocation } from 'react-router-dom';
-import { motion } from 'framer-motion'
+import { motion, useAnimation } from 'framer-motion';
+import { Particles, initParticlesEngine } from "@tsparticles/react";
+
+import particleConfig from './particles.json';
 
 import NavHeader from './components/navbar/NavHeader.jsx';
 import Sidebar from './components/sidebar/Sidebar.jsx';
@@ -8,20 +11,39 @@ import classes from './App.module.css';
 
 const App = () => {
   const path = useLocation();
+  const controls = useAnimation();
+
+  const initParticlesEngine = async (engine) => {
+      await loadSlim(engine);
+    };
+
+  useEffect(() => {
+    controls.start({
+      backgroundPosition: ["70% 70%", "90% 90%", "70% 70%"],
+      transition: {
+        duration: 3,
+        ease: "easeInOut",
+        repeat: Infinity,
+        repeatType: "mirror"
+      }
+    });
+  }, [controls]);
 
   return (
     <div>
       <motion.div className={classes.background}
-        initial={{ background: 'linear-gradient(45deg, #6b0d7a, #ff00ff)' }}
-        animate={{ background: 'linear-gradient(45deg, #ff00ff, #6b0d7a)' }}
-        transition={{ duration: 2, repeat: Infinity, repeatType: 'reverse', ease: 'linear' }}
+      style={{
+        background: 'radial-gradient(125% 130% at 50% 0%, #000000 50%, #67209e)',
+        backgroundSize: '200% 200%',
+      }}
+      animate={controls}
         >
+        <Particles className={classes.particles}
+          params={particleConfig}
+          init={initParticlesEngine}/>
         {path.pathname !== '/' && <Sidebar />}
         {path.pathname !== '/' && <NavHeader />}
         <Outlet />
-        {/*<h1>Hello World!</h1>
-        <ButtonList />
-        <SettingsButton onClick={openSettings}/>*/}
       </ motion.div>
     </ div>
   );
